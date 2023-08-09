d99112cfbf3f80f525bcc45121e13fbb14c05140
WqCat <2416603799@qq.com>
2023-08-09 15:44:19 +0800

    请求参数和验证

--- tutorial/__init__.py
+++ tutorial/__init__.py
@@ -1,1 +1,3 @@
-from .chapter03 import
+from .chapter03 import app03
+from .chapter04 import app04
+from .chapter05 import app05
--- tutorial/chapter05.py
+++ tutorial/chapter05.py
@@ -1,3 +1,3 @@
 from fastapi import APIRouter
 
-app04 = APIRouter()
+app05 = APIRouter()
