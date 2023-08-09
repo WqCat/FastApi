ec703487f505d623a0d8af0bc31bf1d974de2136
WqCat <2416603799@qq.com>
2023-08-09 17:22:59 +0800

    2023.8.9 static目录

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 
+from fastapi.staticfiles import StaticFiles
+
 from tutorial import app03, app04 ,app05
 
 app = FastAPI()
+
+# mount表示将某个目录下一个完全独立的应用挂载过来，这个不会在API交互文档中显示
+app.mount(path="/static", app=StaticFiles(directory='./coronavirus/static', name='static')) # .mount()不要在分路由APIRouter().mount()调用，模板会报错
 
 app.include_router(app03, prefix='/chapter03', tags=['第三章 请求参数和验证'])
 app.include_router(app04, prefix='/chapter04', tags=['第四章 响应处理和FastAPI配置'])
@@ -11,3 +16,4 @@ app.include_router(app05, prefix='/chapter05', tags=['第五章 FastAPI的依赖
 
 if __name__ == '__main__':
     uvicorn.run('run:app', host='127.0.0.1', port=8000, reload=True, workers=1)
+
--- tutorial/__init__.py
+++ tutorial/__init__.py
@@ -1,3 +1,3 @@
-from .chapter03 import app03
-from .chapter04 import app04
-from .chapter05 import app05
+from tutorial.chapter03 import app03
+from tutorial.chapter04 import app04
+from tutorial.chapter05 import app05
